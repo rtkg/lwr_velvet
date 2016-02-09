3d99815ed5062b4d39c7b365d19faee237097d4b
Todor Stoyanov <todor.stoyanov@oru.se>
2016-02-09 15:58:34 +0100

    try again

--- grasping_experiments/scripts/tuneControllers.sh
+++ grasping_experiments/scripts/tuneControllers.sh
@@ -12,7 +12,7 @@ rosservice call /lwr/lwr_velvet_hqp_vel_controller/reset_hqp_control
 #rosservice call /lwr/lwr_velvet_hqp_vel_controller/visualize_task_geometries '{ids: [7, 8, 9, 10, 11, 12]}'
 
 #set the joint setpoint tasks
-rosservice call /lwr/lwr_velvet_hqp_vel_controller/set_tasks '{tasks: [{t_type: 3, priority: 2, name: joint_setpoints, is_equality_task: true, task_frame: "world", ds: 0.0, di: 1.0, dynamics: {d_type: 1, d_data: [-0.4]}, t_links: [{link_frame: "lwr_1_link", geometries: [{g_type: 6, g_data: [1.4]} ]}, {link_frame: "lwr_2_link", geometries: [{g_type: 6, g_data: [0.6]}]}, {link_frame: "lwr_3_link", geometries: [{g_type: 6, g_data: [0.3]} ]}, {link_frame: "lwr_4_link", geometries: [{g_type: 6, g_data: [-1.]} ]}, {link_frame: "lwr_5_link", geometries: [{g_type: 6, g_data: [-0.5]} ]}, {link_frame: "lwr_6_link", geometries: [{g_type: 6, g_data: [0.4]} ]}, {link_frame: "lwr_7_link", geometries: [{g_type: 6, g_data: [0]} ]} ]} ]}'
+rosservice call /lwr/lwr_velvet_hqp_vel_controller/set_tasks '{tasks: [{t_type: 3, priority: 2, name: joint_setpoints, is_equality_task: true, task_frame: "world", ds: 0.0, di: 1.0, dynamics: {d_type: 1, d_data: [-0.4]}, t_links: [{link_frame: "lwr_1_link", geometries: [{g_type: 6, g_data: [1.4]} ]}, {link_frame: "lwr_2_link", geometries: [{g_type: 6, g_data: [0.6]}]}, {link_frame: "lwr_3_link", geometries: [{g_type: 6, g_data: [0.3]} ]}, {link_frame: "lwr_4_link", geometries: [{g_type: 6, g_data: [-1.]} ]}, {link_frame: "lwr_5_link", geometries: [{g_type: 6, g_data: [-0.5]} ]}, {link_frame: "lwr_6_link", geometries: [{g_type: 6, g_data: [0.4]} ]}, {link_frame: "lwr_7_link", geometries: [{g_type: 6, g_data: [0.0]} ]} ]} ]}'
 
 rosservice call /lwr/lwr_velvet_hqp_vel_controller/activate_hqp_control true
 
